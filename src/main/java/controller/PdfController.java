14267b9d35c0dc467c7d32e34aea912a7ad202fd
Bilal <ousssamafoot@gmail.com>
2026-02-05 02:45:20 +0000

    Correction PdfController après redirection vers AccueilController

--- src/main/java/controller/PdfController.java
+++ src/main/java/controller/PdfController.java
@@ -2,7 +2,7 @@ package controller;
 
 import model.ExportPDF;
 import model.Optimisation;
-import view.AccueilView;
+import controller.AccueilController;
 import view.PdfView;
 
 import javax.swing.*;
